b4395ba84d82a83f0b6bfc3a900ba726d55d4d7f
allsunday <kagemichi4221@gmail.com>
2026-06-13 00:00:46 +0000

    chore: archive unreferenced files (reports, snapshots, dup routes) out of repo

--- src/pages/guide.tsx
+++
@@ -1,9 +0,0 @@
-// src/pages/guide.tsx
-export default function GuidePage() {
-  return (
-    <main className="p-8">
-      <h1 className="text-xl font-semibold">Guide</h1>
-      <p className="text-gray-600 mt-2">ガイドの準備中です。</p>
-    </main>
-  );
-}
--- src/pages_legacy/oracle.disabled.tsx
+++
@@ -1,9 +0,0 @@
-// src/pages/oracle.tsx
-export default function OraclePage() {
-  return (
-    <main className="p-8">
-      <h1 className="text-xl font-semibold">Oracle</h1>
-      <p className="text-gray-600 mt-2">占いページのプロトタイプ。</p>
-    </main>
-  );
-}
